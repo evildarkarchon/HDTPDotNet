8b1b5eba6636c1e29f4ebe86c9d016ebb106113d
Andrew Nelson <evildarkarchon@gmail.com>
2024-05-25 18:41:20 -0700

    Initial Project Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HDTPDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
